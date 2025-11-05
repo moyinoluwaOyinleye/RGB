ee502b986cc1319c66868ed098d1fe48d6aa56a6
fayed_1077611 <adamfayed918@gmail.com>
2025-11-05 14:04:44 -0800

    added main.cc

--- RPG-40/main.cc
+++ RPG-40/main.cc
@@ -1,6 +1,20 @@
+#include "/public/read.h" // IWYU pragma: keep
+#include <stdexcept> // IWYU pragma: keep
+#include <vector> // IWYU pragma: keep
+#include <algorithm> // IWYU pragma: keep
+#include <numeric> // IWYU pragma: keep
+#include <cmath>
+#include "/public/colors.h"
+using namespace std;
 //Fill out this comment with your names and which bullet points you did
 //Partners:
 //Bullet Points: 
 //Extra Credit: 
 //URL to cover art and music: 
 
+
+
+int main (){
+	cout << 
+
+}
